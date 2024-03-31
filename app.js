274d644c07567829796f83eab14dcd3e040f1fad
Diana Garcia-Martinez <dianagarcia1316@gmail.com>
2024-03-31 18:42:06 -0400

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const body = document.body;
 //Here I have created a new element
 const div = document.createElement("div");
-body.append(div)
+body.append(div);
 div.textContent = "Here are some moods I feel throughout the day:"
 
 //I had text under the image but I removed it instead
-const pText = document.querySelector("#pText")
-pText.remove(pText)
+// const paraText = document.querySelector("#pText");
+// paraText.remove(pText);
+
+//I added the text back using getElementById
+// let text = document.getElementById('textBubble')
+// text.innerHTML
+
+const changeText = () =>{
+    let text = document.querySelector("#pText");
+    text.innerHTML ='This is me now...' ; 
+    
+}
+
+const toggleImg = () =>{
+    let img2 = document.querySelector('imgContainer2');
+
+}
