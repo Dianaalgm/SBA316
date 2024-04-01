f3dd0af1ab9cfd208f7a0f0abb165b0981dfd2a7
Diana Garcia-Martinez <dianagarcia1316@gmail.com>
2024-03-31 22:45:29 -0400

    fourth commit

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-const body = document.body;
+const Body = document.body ;
 //Here I have created a new element
 const div = document.createElement("div");
-body.append(div);
+Body.append(div);
 div.textContent = "Here are some moods I feel throughout the day:"
 
-//I had text under the image but I removed it instead
-// const paraText = document.querySelector("#pText");
-// paraText.remove(pText);
-
-//I added the text back using getElementById
-// let text = document.getElementById('textBubble')
-// text.innerHTML
 
 const changeText = () =>{
     let text = document.querySelector("#pText");
     text.innerHTML = " This is me now... just joking!(a little) BUT I'm having fun learning all of this for the first time." ; 
-    
 }
 
 //this toggles between two css
@@ -23,3 +15,9 @@ const toggleImg = () =>{
     let img2 = document.querySelector('.imgContainer') ;
     img2.classList.toggle('imgContainer2');
 }
+
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((e) => (
+    e.style.color = 'purple'
+));
+console.log(boxes);
